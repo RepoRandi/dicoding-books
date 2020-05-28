ad0b7a5a4b642946f462a1824c9cd87030805e4a
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-05-28 17:32:26 +0700

    Melengkapi Fungsi insertBook

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -28,6 +28,29 @@ function main() {
 
     const insertBook = (book) => {
         // tuliskan kode di sini!
+        // Membuat instance dari XMLHttpRequest
+        const xhr = new XMLHttpRequest();
+
+        //menetapkan callback jika response sukses dan error
+        xhr.onload = function () {
+            const responseJson = JSON.parse(this.responseText);
+            showResponseMessage(responseJson.message);
+            getBook();
+        }
+
+        xhr.onerror = function () {
+            showResponseMessage();
+        }
+
+        // Membuat POST request dan menetapkan target URL
+        xhr.open("POST", "https://web-server-book-dicoding.appspot.com/add");
+
+        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
+        xhr.setRequestHeader("Content-Type", "application/json");
+        xhr.setRequestHeader("X-Auth-Token", "12345");
+
+        // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
+        xhr.send(JSON.stringify(book));
     };
 
     const updateBook = (book) => {
