32257e5e80f1052a4398e084078eb4df3c9daedd
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-05-30 14:43:43 +0700

    Refactor fungsi insertBook

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -23,29 +23,24 @@ function main() {
 
     const insertBook = (book) => {
         // tuliskan kode di sini!
-        // Membuat instance dari XMLHttpRequest
-        const xhr = new XMLHttpRequest();
-
-        //menetapkan callback jika response sukses dan error
-        xhr.onload = function () {
-            const responseJson = JSON.parse(this.responseText);
-            showResponseMessage(responseJson.message);
-            getBook();
-        }
-
-        xhr.onerror = function () {
-            showResponseMessage();
-        }
-
-        // Membuat POST request dan menetapkan target URL
-        xhr.open("POST", `${baseUrl}/add`);
-
-        // Mementapkan properti Content-Type dan X-Auth-Token pada Header request
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.setRequestHeader("X-Auth-Token", "12345");
-
-        // Mengirimkan request dan menyisipkan JSON.stringify(book) pada body
-        xhr.send(JSON.stringify(book));
+        fetch(`${baseUrl}/add`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "X-Auth-Token": "12345"
+                },
+                body: JSON.stringify(book)
+            })
+            .then(response => {
+                return response.json();
+            })
+            .then(responseJson => {
+                showResponseMessage(responseJson.message);
+                getBook();
+            })
+            .catch(error => {
+                showResponseMessage(error);
+            })
     };
 
     const updateBook = (book) => {
